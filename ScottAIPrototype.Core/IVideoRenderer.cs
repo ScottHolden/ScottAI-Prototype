5d7ea8aa6f174fd2e12880e794e53e388c17e6e1
Scott Holden <scott@nullops.io>
2024-02-26 13:00:47 +1100

    Refactor video implementation out

---
+++ ScottAIPrototype.Core/IVideoRenderer.cs
@@ -0,0 +1,11 @@
+﻿namespace ScottAIPrototype;
+
+public interface IVideoRenderer
+{
+    RenderSize RenderSize { get; }
+    void Init();
+    unsafe void Render(byte* arrayBuffer);
+    void Dispose();
+}
+
+public record RenderSize(uint Width, uint Height);
---
+++ ScottAIPrototype.Video.FixedImage/FixedImageVideoRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ScottAIPrototype;
+
+public class FixedImageVideoRenderer : IVideoRenderer
+{
+    private readonly ILogger _logger;
+    public RenderSize RenderSize { get; }
+    public FixedImageVideoRenderer(RenderSize renderSize, string imagePath, ILogger logger)
+    {
+        RenderSize = renderSize;
+        _logger = logger;
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public void Init()
+    {
+    }
+
+    public unsafe void Render(byte* arrayBuffer)
+    {
+        throw new NotImplementedException();
+    }
+}
